e326d3bccddce387a1e76ed8dbc815330576d00d
João Pedro <joao.guadalupe06@gmail.com>
2026-06-23 20:33:55 -0300

    style: retirada de header do index doc: alteração do readme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,9 +17,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="login" options={{ headerShown: false }} />
-        <Stack.Screen name="feed" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="feed" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
